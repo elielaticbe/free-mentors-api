8260362106d0bf4de70637853f853f2fe13f3e04
elie <elielaticbe@gmail.com>
2019-08-30 14:17:01 -0700

    feat(change user): change user to mentor
    ensure a user is changed to mentor
    
    [Finishes#168091999]

---
+++ models/mentorship.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+const mentorship = [
+  {
+    sessionId: 1,
+    mentorId: 1,
+    mentorEmail: 'samlubangakene@gmail.com',
+    menteeId: 1,
+    questions: 'questions',
+    menteeEmail: 'moisrael@gmail.com',
+    status: 'pending',
+
+  },
+  {
+    sessionId: 2,
+    mentorId: 2,
+    mentorEmail: 'robinoboth@gmail.com',
+    menteeId: 2,
+    questions: 'questions two',
+    menteeEmail: 'mosesisrael@gmail.com',
+    status: 'accepted',
+  },
+  {
+    sessionId: 3,
+    mentorId: 2,
+    mentorEmail: 'samlubangakene@gmail.com',
+    menteeId: 2,
+    questions: 'questions two',
+    menteeEmail: 'mosesisrael@gmail.com',
+    status: 'rejected',
+  },
+];
+
+module.exports = mentorship;
