c7d4ceda340618290ea537a1176e528b42c3ff3f
elie <elielaticbe@gmail.com>
2019-08-30 14:32:49 -0700

    feat(view mentors): get all mentors
    Ensures a user views all mentors
    
    [Finishes#168092001]

---
+++ test/adminTest.js
@@ -0,0 +1,77 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('Admin tests', () => {
+  it('Admin should not be able to change user to mentor when user is admin ', (done) => {
+    const userId = 2;
+    chai.request(app).patch(`/api/v1/admin/user/${userId}`)
+      .end((err, res) => {
+        res.should.have.status(403);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+
+  it('Admin should be able to change user to mentor ', (done) => {
+    const userId = 3;
+    chai.request(app).patch(`/api/v1/admin/user/${userId}`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+  /*
+  it('Admin should not be able to change user to mentor when invalid token', (done) => {
+    const userId = 1;
+    chai.request(app).patch(`/api/v1/user/${userId}`)
+      // eslint-disable-next-line max-len
+      .set('auth', 'yJhbGciOiJIVySWQiOjIsImVtYWlsIjoiYm9iQGdtYWlsLmNvbSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU2NjIzNTEzMywiZXhwIjoxNTY4NjU0MzMzfQ.TwpjQovDnBU3axadqjuaLAUDVPWybj1mAMhxeyCy4p0')
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+
+  it('Admin should not be able to change user to mentor when no token', (done) => {
+    const userId = 1;
+    chai.request(app).patch(`/api/v1/user/${userId}`)
+      .set('auth', '')
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+  */
+  it('Admin should not be able to change user to mentor when user not found', (done) => {
+    const userId = -1;
+    chai.request(app).patch(`/api/v1/admin/user/${userId}`)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+/*
+  it('Admin should not be able to change user to mentor when he is not an admin ', (done) => {
+    const userId = 1;
+    chai.request(app).patch(`/api/v1/user/${userId}`)
+      .set('auth', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoid2lsbHlAZ21haWwuY29tIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU2NjI0MjI5NCwiZXhwIjoxNTY4NjYxNDk0fQ.LDGXyaecz-q-7dG-O71nt3ZK_eh_vS-XJLGyUeZEmz4')
+      .end((err, res) => {
+        res.should.have.status(403);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+*/
+});
